ef5e8708ec196e549df9226b5c369784485c97bc
Chris <chris@fuzzylabs.ai>
2023-04-12 14:45:13 +0100

    Remove redundant comments in deployment pipeline

--- recommendation/pipelines/deploy_recommendation_pipeline.py
+++ recommendation/pipelines/deploy_recommendation_pipeline.py
@@ -16,10 +16,8 @@ def recommendation_deployment_pipeline(
         fetch_model: This step fetches a model artifact from a training pipeline run
         deploy_model: This step deploys the model with Seldon Core
     """
-    # Fetch model from nft_embedding_pipeline
     model = fetch_model()
     
     decision = deployment_trigger(model)
     
-    # Deploy model with Seldon Core
     deploy_model(decision, model)
