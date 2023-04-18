b8a344fa390f73a0785b08a7d804ba0c30bedc90
Oscar <osw282@gmail.com>
2023-04-18 13:14:41 +0100

    update based on review comments

--- recommendation/setup.sh
+++ recommendation/setup.sh
@@ -1,17 +1,17 @@
-# #!/bin/bash
-echo "Installing example requirements (see requirements.txt)..."
-{
-    pip install -r requirements.txt
+#!/bin/bash
+# echo "Installing example requirements (see requirements.txt)..."
+# {
+#     pip install -r requirements.txt
     
-    zenml integration install mlflow azure kubernetes seldon -y
-} >> setup_out.log
+#     zenml integration install mlflow azure kubernetes seldon -y
+# } >> setup_out.log
 
-if [[ ! -f .matcha/infrastructure/matcha.state ]]
-then
-    echo "Error: The file .matcha/infrastructure/matcha.state does not exist!"
-    echo "Ensure that you have run 'matcha provision' in this directory and all cloud resources have been provisioned."
-    exit 1
-fi
+# if [[ ! -f .matcha/infrastructure/matcha.state ]]
+# then
+#     echo "Error: The file .matcha/infrastructure/matcha.state does not exist!"
+#     echo "Ensure that you have run 'matcha provision' in this directory and all cloud resources have been provisioned."
+#     exit 1
+# fi
 
 
 get_state_value() {
@@ -55,8 +55,8 @@ variables=(
 for variable in "${variables[@]}"; do
   value=${!variable}
   if [ -z "$value" ]; then
-    echo "One or more required variable is empty, please try running matcha provision again."
-    break
+    echo "The required variable $variable is empty, please try running matcha provision again."
+    exit 1
   fi
 done
 
@@ -70,48 +70,26 @@ echo "Setting up ZenML..."
     
     zenml connect --url="$zenserver_url" --username="$zenserver_username" --password="$zenserver_password" --no-verify-ssl
 
-    # # Check if the stack is already registered and removed if found
-    if zenml stack list --name=recommendation_example_cloud_stack | grep -q "items found for the applied filters."; then
-        echo "Found and removing existing stack named recommendation_example_cloud_stack."
-        zenml stack set default
-        yes | zenml stack delete recommendation_example_cloud_stack
-    fi
-    
-    # Check if the az_secret already exists and remove it if found
-    if zenml secret list | grep -q "az_secret"; then
-        echo "Found and removing existing secret component named az_secret."
-        yes | zenml secret delete az_secret
-    fi
+    cleanup_resources() {
+        local resources=("stack:$1" "secret:az_secret" "container-registry:acr_registry" "experiment-tracker:mlflow_experiment_tracker" "artifact-store:az_store" "orchestrator:k8s_orchestrator" "model-deployer:seldon_deployer")
+            
+        # Special case for stack as zenml stack list returns a table which the name of stack is separated into multiple lines
+        if zenml stack list --name="$1" | grep -q "items found for the applied filters."; then
+            echo "Found and removing existing stack named $1"
+            zenml stack set default
+            yes | zenml stack delete "$1"
+        fi
 
-    # Check if acr_registry already exists
-    if zenml container-registry list | grep -q "acr_registry"; then
-        echo "Found and removing existing container-registry component named acr_registry."
-        yes | zenml container-registry delete acr_registry
-    fi
+        for resource in "${resources[@]}"; do
+            type="${resource%%:*}" # remove everything after :, inclusive
+            name="${resource#*:}" # remove everything before :, inclusive
+            if zenml "$type" list | grep -q "$name"; then
+                yes | zenml "$type" delete "$name"
+            fi
+        done
+    }
 
-    # Check if mlflow_experiment_tracker already exists
-    if zenml experiment-tracker list | grep -q "mlflow_experiment_tracker"; then
-        echo "Found and removing existing experiment-tracker component named mlflow_experiment_tracker."
-        yes | zenml experiment-tracker delete mlflow_experiment_tracker
-    fi
-
-    # Check if az_store already exists
-    if zenml artifact-store list | grep -q "az_store"; then
-        echo "Found and removing existing artifact-store component named az_store."
-        yes | zenml artifact-store delete az_store
-    fi
-
-    # Check if k8s_orchestrator already exists
-    if zenml orchestrator list | grep -q "k8s_orchestrator"; then
-        echo "Found and removing existing orchestrator component named k8s_orchestrator."
-        yes | zenml orchestrator delete k8s_orchestrator
-    fi
-
-    # Check if seldon_deployer already exists
-    if zenml model-deployer list | grep -q "seldon_deployer"; then
-        echo "Found and removing existing model-deployer component named seldon_deployer."
-        yes | zenml model-deployer delete seldon_deployer   
-    fi
+    cleanup_resources "recommendation_example_cloud_stack"
 
     zenml connect --url="$zenserver_url" --username="$zenserver_username" --password="$zenserver_password" --no-verify-ssl
     zenml secret create az_secret --connection_string="$zenml_connection_string"
