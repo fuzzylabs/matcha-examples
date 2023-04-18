dbb9ccc29c84a8c68a6133fe3c03b9906839f046
Chris <32800386+Christopher-Norman@users.noreply.github.com>
2023-04-18 11:52:31 +0100

    [RPD-94] Add the matcha run deploy changes to the example repo (#18)

--- recommendation/run.py
+++ recommendation/run.py
@@ -44,17 +44,23 @@ def run_deployment_pipeline():
     
 @click.command()
 @click.option("--train", "-t", is_flag=True, help="Run training pipeline")
-def main(train: bool):
+@click.option("--deploy", "-d", is_flag=True, help="Run the deployment pipeline")
+def main(train: bool, deploy: bool):
     """Run all pipelines.
     
     args:
         train (bool): Flag for running the training pipeline.
+        deploy (bool): Flag for running the deployment pipeline.
     """
     if train:
         logger.info("Running recommendation training pipeline.")
         run_recommendation_pipeline()
     
-    if not train:
+    if deploy:
+        logger.info("Running deployment pipeline.")
+        run_deployment_pipeline()
+    
+    if (not train) and (not deploy):
         logger.info("Running recommendation training pipeline.")
         run_recommendation_pipeline()
         
