e8628db11691f62c8893705ec8f4400b458adaea
dudeperf3ct <dudeperf3ct@users.noreply.github.com>
2023-04-27 17:05:14 +0530

    remove input text

--- llm/app/llm_demo.py
+++ llm/app/llm_demo.py
@@ -8,16 +8,7 @@ def fetch_summary():
 
 
 def main():
-    input_text = st.text_area(label='Text to summarize',
-                    value='''Fuzzy Labs Limited (company no. 11762819) (“us”, “we”, or “our”) operates https://fuzzylabs.ai (the “Site”).
-This page informs you of our policies regarding the collection, use and disclosure of Personal Information we receive from users of the Site.
-We use your Personal Information only for providing and improving the Site.
-By using the Site, you agree to the collection and use of information in accordance with this policy. Information Collection And Use
-While using our Site, we may ask you to provide us with certain personally identifiable information that can be used to contact or identify you.
-Personally identifiable information may include, but is not limited to your name and email address (“Personal Information”).
-''',
-        height=400
-        )
+    input_text = st.text_area(label='Text to summarize', height=400)
 
 
     summarized_text = st.text_area(label="Summarized Text",
