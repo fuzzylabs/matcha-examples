32e139da60101c90e2aaa94ac66ff3e6e7905dd3
dudeperf3ct <dudeperf3ct@users.noreply.github.com>
2023-05-05 16:44:26 +0530

    add function to fetch prediction

--- llm/app/llm_demo.py
+++ llm/app/llm_demo.py
@@ -1,7 +1,31 @@
 import streamlit as st
 import json
+from zenml.integrations.seldon.model_deployers.seldon_model_deployer import (
+    SeldonModelDeployer,
+)
 
-st.title('LLM Summarization Demo')
+st.title("LLM Summarization Demo")
+
+PIPELINE_NAME = "llm_deployment_pipeline"
+PIPELINE_STEP = "deploy_model"
+MODEL_NAME = "seldon-llm-custom-model"
+
+
+def _get_prediction_endpoint() -> str:
+    """Get the endpoint for the currently deployed LLM model.
+
+    Returns:
+        str: the url endpoint.
+    """
+    model_deployer = SeldonModelDeployer.get_active_model_deployer()
+
+    deployed_services = model_deployer.find_model_server(
+        pipeline_name=PIPELINE_NAME,
+        pipeline_step_name=PIPELINE_STEP,
+        model_name=MODEL_NAME,
+    )
+
+    return deployed_services[0].prediction_url
 
 
 def fetch_summary(txt: str) -> str:
@@ -13,10 +37,14 @@ def fetch_summary(txt: str) -> str:
     Returns:
         str: Summarized text
     """
-    ...
+    seldon_url = _get_prediction_endpoint()
+    if seldon_url is None:
+        st.write("Hmm, seldon endpoint is not provisioned yet!")
+    else:
+        pass
 
 
-def read_examples(file_path: str = 'example.json') -> dict:
+def read_examples(file_path: str = "example.json") -> dict:
     """Read sample examples for LLM summarization demo.
 
     Args:
@@ -25,7 +53,7 @@ def read_examples(file_path: str = 'example.json') -> dict:
     Returns:
         dict: Dictionary containing examples.
     """
-    with open(file_path, 'r') as myfile:
+    with open(file_path, "r") as myfile:
         data = myfile.read()
     return json.loads(data)
 
@@ -40,23 +68,22 @@ def switch_examples(data: dict) -> str:
         str: Input text to summarize.
     """
     pages = ["Example 1", "Example 2", "Example 3"]
-    page = st.radio('Test Examples', pages)
+    page = st.radio("Test Examples", pages)
 
     if page == "Example 1":
-        text = data['example1']
+        text = data["example1"]
 
     if page == "Example 2":
-        text = data['example2']
+        text = data["example2"]
 
     if page == "Example 3":
-        text = data['example3']
+        text = data["example3"]
 
-    input_text = st.text_area(label='Text to summarize', value=text, height=400)
+    input_text = st.text_area(label="Text to summarize", value=text, height=400)
     return input_text
 
 
 def main():
-
     data = read_examples(file_path="example.json")
 
     txt = switch_examples(data)
@@ -64,11 +91,12 @@ def main():
     result = st.button(label="Ready")
 
     if result:
-        summarized_text = st.text_area(label="Summarized Text",
-                                       value="Dummy summary",  # Replace with fetch_summary(txt)
-                                       height=200
-                                       )
+        summarized_text = st.text_area(
+            label="Summarized Text",
+            value=fetch_summary(txt),
+            height=200,
+        )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
