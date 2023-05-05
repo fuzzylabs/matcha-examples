0140cd9696b0840065151d946a5955ead8240634
dudeperf3ct <dudeperf3ct@users.noreply.github.com>
2023-05-05 16:02:12 +0530

    remove last step from deployment pipeline

--- llm/pipelines/llm_deployment_pipeline.py
+++ llm/pipelines/llm_deployment_pipeline.py
@@ -3,20 +3,15 @@ from zenml.pipelines import pipeline
 
 
 @pipeline
-def llm_deployment_pipeline(fetch_trained_model, deploy_model, get_prediction_endpoint):
+def llm_deployment_pipeline(fetch_trained_model, deploy_model):
     """Pipeline to deploy fine-tuned LLM model.
 
     Args:
         fetch_trained_model : A step to fetch path to trained model and decision to deploy the model.
         deploy_model: A step to deploy LLM model using Seldon.
-        get_prediction_endpoint: A step to fetch seldon endpoint URL.
     """
-    get_prediction_endpoint.after(deploy_model)
-
     # Fetch the trained model path, tokenizer path and decision
     model_uri, tokenizer_uri, decision = fetch_trained_model()
 
     # Deploy the model
     deploy_model(decision, model_uri, tokenizer_uri)
-
-    get_prediction_endpoint()
